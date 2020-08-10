890b74796bde846c029916c6941eac450cdd6298
Zoli <zolee1@gmail.com>
2020-08-10 19:47:07 +0200

    20200810

---
+++ hamburgerezo/js/main.js
@@ -0,0 +1,5 @@
+
+$('#myTab a[href="#profile"]').tab('show') // Select tab by name
+$('#myTab li:first-child a').tab('show') // Select first tab
+$('#myTab li:last-child a').tab('show') // Select last tab
+$('#myTab li:nth-child(3) a').tab('show') // Select third tab
